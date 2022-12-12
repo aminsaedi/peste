66af3ebd5dc78e0d10244099a8fa4affe4a0b499
Amin Saedi <amin.di.sam@gmail.com>
2022-12-11 22:38:55 -0500

    new data using iri-victims/women-life-freedom dataset

--- components/AppCard.tsx
+++ components/AppCard.tsx
@@ -22,39 +22,38 @@ export default function AppCard({ record }: { record: any }) {
   const [open, setOpen] = useState(false);
 
   return (
-    <React.Fragment>
-      <Grid item xs={2} sm={4} md={4}>
-        <Card>
-          <CardMedia>
-            <Image
-              src="https://picsum.photos/300/300"
-              width={300}
-              height={300}
-              alt="random image"
-              style={{ width: "100%" }}
-            />
-          </CardMedia>
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {record.title}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {record.subtitle}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button onClick={() => setOpen(true)} size="small">
-              Learn More
-            </Button>
-          </CardActions>
-        </Card>
-      </Grid>
+    <Grid item xs={2} sm={4} md={4}>
+      <Card style={{ height: "100%" }}>
+        <CardMedia>
+          <Image
+            src={`https://picsum.photos/seed/${record.id}/200/300`}
+            width={300}
+            height={300}
+            alt="random image"
+            style={{ width: "100%" }}
+          />
+        </CardMedia>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {record.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {record.subtitle || "بدون توضیحات"}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button onClick={() => setOpen(true)} size="small">
+            جزئیات بیشتر
+          </Button>
+        </CardActions>
+      </Card>
       <Dialog
         open={open}
         onClose={() => setOpen(false)}
         scroll="paper"
         aria-labelledby="scroll-dialog-title"
         aria-describedby="scroll-dialog-description"
+        fullWidth
       >
         <DialogTitle id="scroll-dialog-title">{record.title}</DialogTitle>
         <DialogContent dividers>
@@ -64,9 +63,9 @@ export default function AppCard({ record }: { record: any }) {
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setOpen(false)}>Cancel</Button>
+          <Button onClick={() => setOpen(false)}>بازگشت</Button>
         </DialogActions>
       </Dialog>
-    </React.Fragment>
+    </Grid>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,35 +2,42 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "vazirmatn/Vazirmatn-font-face.css";
 
 import Head from "next/head";
 
 import Grid from "@mui/material/Grid";
 import Paper from "@mui/material/Paper";
 
-import { styled } from "@mui/material/styles";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import AppCard from "../components/AppCard";
 import { getSortedPostsData } from "../lib/posts";
 
-const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-}));
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Vazirmatn", "sans-serif"].join(","),
+  },
+  direction: "rtl",
+});
 
 export default function Home({ allPostsData }: { allPostsData: any[] }) {
+  // document.dir = "rtl";
   return (
-    <div>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>Zan Zendegi Azadi</title>
         <link rel="icon" href="/favicon.ico" />
+        {/* <link
+          href="https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.000/Vazirmatn-font-face.css"
+          rel="stylesheet"
+          type="text/css"
+        /> */}
       </Head>
 
       <main style={{ padding: "1rem" }}>
         <Grid
           container
+          alignItems="stretch"
           spacing={{ xs: 2, md: 3 }}
           columns={{ xs: 2, sm: 8, md: 12 }}
         >
@@ -39,7 +46,7 @@ export default function Home({ allPostsData }: { allPostsData: any[] }) {
           ))}
         </Grid>
       </main>
-    </div>
+    </ThemeProvider>
   );
 }
 
